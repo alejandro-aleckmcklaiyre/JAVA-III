0045f148a0196a3ac06f39fe3f24730191bed993
alejandro-aleckmcklaiyre <aleck.alejandro04@gmail.com>
2025-10-24 16:26:37 +0800

    Upload Activity 10

---
+++ Activity_10/src/descendingprinter/DescendingPrinter.java
@@ -0,0 +1,22 @@
+package descendingprinter;
+
+import java.util.Scanner;
+
+public class DescendingPrinter {
+
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter a number: ");
+		int firstInput = input.nextInt();
+		
+		for (int baseValue = 1; firstInput >= baseValue; firstInput--) {
+			System.out.println(firstInput);
+		}
+			System.out.println("Loop Complete!");
+		  
+		input.close();
+ }
+
+}
